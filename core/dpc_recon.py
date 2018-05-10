a3c7ec920e4db09be51d5411a44ef504c3efc60f
sungsoo ha <hasungsoo@gmail.com>
2018-05-10 18:33:15 -0400

    update gui interface except advanced options

--- core/dpc_recon.py
+++ core/dpc_recon.py
@@ -2,25 +2,57 @@ from PyQt4 import  QtCore
 from datetime import datetime
 from core.dpc_param import Param
 
-def test_api(param:Param, update_fcn=None):
-    for i in range(param.n_iterations):
-        print(datetime.now()) # dummy output
-
-        # pass notification to gui
-        if update_fcn is not None:
-            update_fcn(i+1)
-
 try:
     #from .ptycho.recon_ptycho_gui import recon_gui
     #from mpi4py import MPI
     import sys, os
     import pickle     # test, dump param into disk
     import subprocess # test, call mpirun from shell
-    #recon_api = recon_gui
+
+    def recon_api(param:Param, update_fcn=None):
+        # self.param.gpus = [0] # GPU #1, for test purpose
+        with open('.dpc_param.pkl', 'wb') as output:
+            pickle.dump(param, output, pickle.HIGHEST_PROTOCOL)  # dump param into disk and let children read it back
+            print("pickle dumped")
+
+        # working version
+        mpirun_command = ["mpirun", "-n", str(len(param.gpus)), "python", "./core/ptycho/recon_ptycho_gui.py"]
+        with subprocess.Popen(mpirun_command,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.STDOUT,
+                              env=dict(os.environ, mpi_warn_on_fork='0')) as run_ptycho:
+            while True:
+                output = run_ptycho.stdout.readline()
+                if output == b'' and run_ptycho.poll() is not None:
+                    break
+                if output:
+                    print(output.decode('utf-8'))
+            # does GUI wanna know the return value = run_ptycho.poll()?
+
+        # clean up temp file
+        os.remove('.dpc_param.pkl')
+
+        ########## test code below... #########
+
+        # result = subprocess.run(["mpirun", "-n", str(len(self.param.gpus)), "python", "./core/ptycho/recon_ptycho_gui.py"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        # print(result)
+        # print(result.stdout)
+        # print(result.stdout.decode('utf-8')) # attach the stdout to GUI?
+
+        # comm = MPI.COMM_WORLD.Spawn(sys.executable, args = ["./core/ptycho/recon_ptycho_gui.py"], maxprocs = len(self.param.gpus))
+        # comm.Disconnect()
+
 except ImportError as ex:
     print('[!] Unable to import dpc recon packages')
     print('[!] (import error: {})'.format(ex))
-    recon_api = test_api
+
+    def test_api(param: Param, update_fcn=None):
+        for i in range(param.n_iterations):
+            print(datetime.now())  # dummy output
+
+            # pass notification to gui
+            if update_fcn is not None:
+                update_fcn(i + 1)
 
 
 class DPCReconWorker(QtCore.QThread):
@@ -33,37 +65,7 @@ class DPCReconWorker(QtCore.QThread):
 
     def run(self):
         print('DPC thread started')
-        try: 
-            #self.param.gpus = [0] # GPU #1, for test purpose
-            with open('.dpc_param.pkl', 'wb') as output:
-               pickle.dump(self.param, output, pickle.HIGHEST_PROTOCOL) # dump param into disk and let children read it back
-               print("pickle dumped")
-
-            # working version
-            mpirun_command = ["mpirun", "-n", str(len(self.param.gpus)), "python", "./core/ptycho/recon_ptycho_gui.py"]
-            with subprocess.Popen(mpirun_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, \
-                                  env=dict(os.environ, mpi_warn_on_fork='0')) as run_ptycho:
-               while True:
-                  output = run_ptycho.stdout.readline()
-                  if output == b'' and run_ptycho.poll() is not None:
-                      break
-                  if output:
-                      print(output.decode('utf-8'))
-               # does GUI wanna know the return value = run_ptycho.poll()?
-
-            # clean up temp file
-            os.remove('.dpc_param.pkl')
-
-            ########## test code below... #########
-            
-            #result = subprocess.run(["mpirun", "-n", str(len(self.param.gpus)), "python", "./core/ptycho/recon_ptycho_gui.py"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            #print(result)
-            #print(result.stdout)
-            #print(result.stdout.decode('utf-8')) # attach the stdout to GUI?
-
-            #comm = MPI.COMM_WORLD.Spawn(sys.executable, args = ["./core/ptycho/recon_ptycho_gui.py"], maxprocs = len(self.param.gpus))
-            #comm.Disconnect()
-
-            #recon_api(self.param, self.update_signal.emit)
+        try:
+            recon_api(self.param, self.update_signal.emit)
         finally:
             print('finally?')
--- dpc_gpu_gui.py
+++ dpc_gpu_gui.py
@@ -19,8 +19,17 @@ class MainWindow(QtGui.QMainWindow, ui_dpc.Ui_MainWindow):
         #
         # connect
         #
+        qt_connect(self.btn_load_probe , qt_signal('clicked()'), self.loadProbe)
+        qt_connect(self.btn_load_object, qt_signal('clicked()'), self.loadObject)
+        qt_connect(self.ck_mode_flag, qt_signal('clicked()'), self.updateModeFlg)
+        qt_connect(self.ck_multislice_flag, qt_signal('clicked()'), self.updateMultiSliceFlg)
+        qt_connect(self.ck_gpu_flag, qt_signal('clicked()'), self.updateGpuFlg)
+
         qt_connect(self.btn_recon_start, qt_signal('clicked()'), self.start)
         qt_connect(self.btn_recon_stop , qt_signal('clicked()'), self.stop)
+
+        self.btn_gpu_all = [self.btn_gpu_0, self.btn_gpu_1, self.btn_gpu_2, self.btn_gpu_3]
+
 
         #
         # init.
@@ -30,115 +39,102 @@ class MainWindow(QtGui.QMainWindow, ui_dpc.Ui_MainWindow):
         else:
             self.param = param
         self._dpc_gpu_thread = None
-        #self.update_gui_from_param()
+        self.update_gui_from_param()
+        self.updateModeFlg()
+        self.updateMultiSliceFlg()
+        self.updateGpuFlg()
 
     def update_param_from_gui(self):
         p = self.param
         p.scan_num = str(self.le_scan_num.text())
-        p.sign = str(self.le_sign.text())
+        p.detectorkind = str(self.cb_detectorkind.currentText())
+        p.frame_num = int(self.sp_fram_num.value())
+
+        p.xray_energy = float(self.sp_xray_energy.value())
+        p.detector_distance = float(self.sp_detector_distance.value())
+        p.x_arr_size = float(self.sp_x_arr_size.value())
+        p.x_step_size = float(self.sp_x_step_size.value())
+        p.x_scan_range = float(self.sp_x_scan_range.value())
+        p.y_arr_size = float(self.sp_y_arr_size.value())
+        p.y_step_size = float(self.sp_y_step_size.value())
+        p.y_scan_range = float(self.sp_y_scan_range.value())
+        p.scan_type = str(self.cb_scan_type.currentText())
+        p.num_points = int(self.sp_num_points.value())
+
         p.n_iterations = int(self.sp_n_iterations.value())
-        p.p_flag = self.ck_p_flag.isChecked()
-        p.distance = float(self.sp_distance.value())
-        p.mode_flag = self.ck_mode_flag.isChecked()
-        p.gpu_flag = self.ck_gpu_flag.isChecked()
-        p.mesh_flag = self.ck_mesh_flag.isChecked()
-        p.start_ave = float(self.sp_start_ave.value())
-        p.nth = int(self.sp_nth.value())
-        p.ccd_pixel_um = float(self.sp_ccd_pixel_um.value())
-        p.cal_scan_pattern_flag = self.ck_cal_scal_pattern_flag.isChecked()
         p.alg_flag = str(self.cb_alg_flag.currentText())
-        p.ml_mode = str(self.cb_ml_mode.currentText())
         p.alg2_flag = str(self.cb_alg2_flag.currentText())
         p.alg_percentage = float(self.sp_alg_percentage.value())
-        p.bragg_flag = self.ck_bragg_flag.isChecked()
-        p.bragg_theta = float(self.sp_bragg_theta.value())
-        p.bragg_gamma = float(self.sp_bragg_gamma.value())
-        p.bragg_delta = float(self.sp_bragg_delta.value())
-        p.amp_max = float(self.sp_amp_max.value())
-        p.amp_min = float(self.sp_amp_min.value())
-        p.pha_max = float(self.sp_pha_max.value())
-        p.pha_min = float(self.sp_pha_min.value())
-        p.pc_flag = self.ck_pc_flag.isChecked()
-        p.pc_sigma = float(self.sp_pc_sigma.value())
-        p.pc_alg = str(self.cb_pc_alg.currentText())
-        p.pc_kernel_n = int(self.sp_pc_kernel_n.value())
-        p.alpha = float(self.sp_alpha.value())
-        p.beta = float(self.sp_beta.value())
-        p.save_tmp_pic_flag = self.ck_save_tmp_pic_flag.isChecked()
-        p.prb_mode_num = int(self.sp_prb_mode_num.value())
-        p.obj_mode_num = int(self.sp_obj_mode_num.value())
-        p.position_correction_flag = self.ck_position_correction_flag.isChecked()
-        p.position_correction_start = int(self.sp_position_correction_start.value())
-        p.position_correction_step = int(self.sp_position_correction_step.value())
+
+        p.init_prb_flag = self.ck_init_prb_flag.isChecked()
+        p.init_obj_flag = self.ck_init_obj_flag.isChecked()
+
+        p.mode_flag = self.ck_mode_flag.isChecked()
+        p.prb_mode_num = self.sp_prb_mode_num.value()
+        p.obj_mode_num = self.sp_obj_mode_num.value()
+
         p.multislice_flag = self.ck_multislice_flag.isChecked()
         p.slice_num = int(self.sp_slice_num.value())
-        p.slice_spacing_m = float(self.sp_slice_spacing_m.value())
-        p.start_update_probe = int(self.sp_start_update_probe.value())
-        p.start_update_object = int(self.sp_start_update_object.value())
-        p.init_obj_flag = self.ck_init_obj_flag.isChecked()
-        p.init_obj_dpc_flag = self.ck_init_obj_dpc_flag.isChecked()
-        p.prb_center_flag = self.ck_prb_center_flag.isChecked()
-        p.init_prb_flag = self.ck_init_prb_flag.isChecked()
-        p.mask_prb_flag = self.ck_mask_prb_flag.isChecked()
-        p.dm_version = int(self.sp_dm_version.value())
-        p.sf_flag = self.ck_sf_flag.isChecked()
-        p.ms_pie_flag = self.ck_ms_pie_flag.isChecked()
-        p.weak_obj_flag = self.ck_weak_obj_flag.isChecked()
-        p.processes = int(self.sp_processes.value())
+        p.slice_spacing_m = float(self.sp_slice_spacing_m.value() * 1e-6)
+
+        p.amp_min = float(self.sp_amp_min.value())
+        p.amp_max = float(self.sp_amp_max.value())
+        p.pha_min = float(self.sp_pha_min.value())
+        p.pha_max = float(self.sp_pha_max.value())
+
+        p.gpu_flag = self.ck_gpu_flag.isChecked()
+        gpus = []
+        for btn_gpu, id in zip(self.btn_gpu_all, range(len(self.btn_gpu_all))):
+            if btn_gpu.isChecked():
+                gpus.append(id)
+        p.gpus = gpus
+
 
     def update_gui_from_param(self):
         p = self.param
         self.le_scan_num.setText(p.scan_num)
-        self.le_sign.setText(p.sign)
-        self.sp_n_iterations.setValue(p.n_iterations)
-        self.ck_p_flag.setChecked(p.p_flag)
-        self.sp_distance.setValue(p.distance)
-        self.ck_mode_flag.setChecked(p.mode_flag)
-        self.ck_gpu_flag.setChecked(p.gpu_flag)
-        self.ck_mesh_flag.setChecked(p.mesh_flag)
-        self.sp_start_ave.setValue(p.start_ave)
-        self.sp_nth.setValue(p.nth)
-        self.sp_ccd_pixel_um.setValue(p.ccd_pixel_um)
-        self.ck_cal_scal_pattern_flag.setChecked(p.cal_scan_pattern_flag)
+        self.cb_detectorkind.setCurrentIndex(p.get_detector_kind_index())
+        self.sp_fram_num.setValue(int(p.frame_num))
+
+        self.sp_xray_energy.setValue(float(p.xray_energy))
+        self.sp_detector_distance.setValue(float(p.detector_distance))
+        self.sp_x_arr_size.setValue(float(p.x_arr_size))
+        self.sp_x_step_size.setValue(float(p.x_step_size))
+        self.sp_x_scan_range.setValue(float(p.x_scan_range))
+        self.sp_y_arr_size.setValue(float(p.y_arr_size))
+        self.sp_y_step_size.setValue(float(p.y_step_size))
+        self.sp_y_scan_range.setValue(float(p.y_scan_range))
+        self.cb_scan_type.setCurrentIndex(p.get_scan_type_index())
+        self.sp_num_points.setValue(int(p.num_points))
+
+        self.sp_n_iterations.setValue(int(p.n_iterations))
         self.cb_alg_flag.setCurrentIndex(p.get_alg_flg_index())
-        self.cb_ml_mode.setCurrentIndex(p.get_ml_model_index())
         self.cb_alg2_flag.setCurrentIndex(p.get_alg2_flg_index())
-        self.sp_alg_percentage.setValue(p.alg_percentage)
-        self.ck_bragg_flag.setChecked(p.bragg_flag)
-        self.sp_bragg_theta.setValue(p.bragg_theta)
-        self.sp_bragg_gamma.setValue(p.bragg_gamma)
-        self.sp_bragg_delta.setValue(p.bragg_delta)
-        self.sp_amp_max.setValue(p.amp_max)
-        self.sp_amp_min.setValue(p.amp_min)
-        self.sp_pha_max.setValue(p.pha_max)
-        self.sp_pha_min.setValue(p.pha_min)
-        self.ck_pc_flag.setChecked(p.pc_flag)
-        self.sp_pc_sigma.setValue(p.pc_sigma)
-        self.cb_pc_alg.setCurrentIndex(p.get_pc_alg_index())
-        self.sp_pc_kernel_n.setValue(p.pc_kernel_n)
-        self.sp_alpha.setValue(p.alpha)
-        self.sp_beta.setValue(p.beta)
-        self.ck_save_tmp_pic_flag.setChecked(p.save_tmp_pic_flag)
-        self.sp_prb_mode_num.setValue(p.prb_mode_num)
-        self.sp_obj_mode_num.setValue(p.obj_mode_num)
-        self.ck_position_correction_flag.setChecked(p.position_correction_flag)
-        self.sp_position_correction_start.setValue(p.position_correction_start)
-        self.sp_position_correction_step.setValue(p.position_correction_step)
-        self.ck_multislice_flag.setChecked(p.multislice_flag)
-        self.sp_slice_num.setValue(p.slice_num)
-        self.sp_slice_spacing_m.setValue(p.slice_spacing_m)
-        self.sp_start_update_probe.setValue(p.start_update_probe)
-        self.sp_start_update_object.setValue(p.start_update_object)
-        self.ck_init_obj_flag.setChecked(p.init_obj_flag)
-        self.ck_init_obj_dpc_flag.setChecked(p.init_obj_dpc_flag)
-        self.ck_prb_center_flag.setChecked(p.prb_center_flag)
+        self.sp_alg_percentage.setValue(float(p.alg_percentage))
+
         self.ck_init_prb_flag.setChecked(p.init_prb_flag)
-        self.ck_mask_prb_flag.setChecked(p.mask_prb_flag)
-        self.sp_dm_version.setValue(p.dm_version)
-        self.ck_sf_flag.setChecked(p.sf_flag)
-        self.ck_ms_pie_flag.setChecked(p.ms_pie_flag)
-        self.ck_weak_obj_flag.setChecked(p.weak_obj_flag)
-        self.sp_processes.setValue(p.processes)
+        self.le_prb_path.setText(str(p.prb_filename or ''))
+
+        self.ck_init_obj_flag.setChecked(p.init_obj_flag)
+        self.le_obj_path.setText(str(p.obj_filename or ''))
+
+        self.ck_mode_flag.setChecked(p.mode_flag)
+        self.sp_prb_mode_num.setValue(int(p.prb_mode_num))
+        self.sp_obj_mode_num.setValue(int(p.obj_mode_num))
+
+        self.ck_multislice_flag.setChecked(p.multislice_flag)
+        self.sp_slice_num.setValue(int(p.slice_num))
+        self.sp_slice_spacing_m.setValue(p.get_slice_spacing_m())
+
+        self.sp_amp_max.setValue(float(p.amp_max))
+        self.sp_amp_min.setValue(float(p.amp_min))
+        self.sp_pha_max.setValue(float(p.pha_max))
+        self.sp_pha_min.setValue(float(p.pha_min))
+
+        self.ck_gpu_flag.setChecked(p.gpu_flag)
+        for btn_gpu, id in zip(self.btn_gpu_all, range(len(self.btn_gpu_all))):
+            btn_gpu.setChecked(id in p.gpus)
+
 
     def start(self):
         if self._dpc_gpu_thread is not None and self._dpc_gpu_thread.isFinished():
@@ -158,6 +154,44 @@ class MainWindow(QtGui.QMainWindow, ui_dpc.Ui_MainWindow):
 
     def update_recon_step(self, it):
         self.recon_bar.setValue(it)
+
+    def loadProbe(self):
+        filename = QtGui.QFileDialog.getOpenFileName(self, 'Open probe file', filter="(*.npy)")
+        if filename is not None and len(filename) > 0:
+            prb_filename = os.path.basename(filename)
+            prb_dir = filename[:(len(filename)-len(prb_filename))]
+            self.param.set_prb_path(prb_dir, prb_filename)
+            self.le_prb_path.setText(prb_filename)
+            self.ck_init_prb_flag.setChecked(False)
+
+    def loadObject(self):
+        filename = QtGui.QFileDialog.getOpenFileName(self, 'Open object file', filter="(*.npy)")
+        if filename is not None and len(filename) > 0:
+            obj_filename = os.path.basename(filename)
+            obj_dir = filename[:(len(filename)-len(obj_filename))]
+            self.param.set_obj_path(obj_dir, obj_filename)
+            self.le_obj_path.setText(obj_filename)
+            self.ck_init_obj_flag.setChecked(False)
+
+    def updateModeFlg(self):
+        mode_flag = self.ck_mode_flag.isChecked()
+        self.sp_prb_mode_num.setEnabled(mode_flag)
+        self.sp_obj_mode_num.setEnabled(mode_flag)
+        self.param.mode_flag = mode_flag
+
+    def updateMultiSliceFlg(self):
+        flag = self.ck_multislice_flag.isChecked()
+        self.sp_slice_num.setEnabled(flag)
+        self.sp_slice_spacing_m.setEnabled(flag)
+        self.param.multislice_flag = flag
+
+    def updateGpuFlg(self):
+        flag = self.ck_gpu_flag.isChecked()
+        self.btn_gpu_0.setEnabled(flag)
+        self.btn_gpu_1.setEnabled(flag)
+        self.btn_gpu_2.setEnabled(flag)
+        self.btn_gpu_3.setEnabled(flag)
+
 
     @QtCore.pyqtSlot(str)
     def on_stdout_message(self, message):
