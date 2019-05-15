feeea9c952dd2252c88549095b6ed19e52ff8e88
Leo Fang <leofang@bnl.gov>
2019-05-14 20:56:17 -0400

    added clean_shared_memory() to address #40

--- nsls2ptycho/core/utils.py
+++ nsls2ptycho/core/utils.py
@@ -1,9 +1,16 @@
+import getpass
+import os
+from pwd import getpwuid
+import sys
+
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
+
 from nsls2ptycho.core.ptycho.utils import split
 
 
+# DEPRECARED: migrated to nsls2ptycho.core.widgets.mplcanvas
 def plot_point_process_distribution(pts, mpi_size, colormap=cm.jet):
     '''
     Plot N scanning points in mpi_size different colors
@@ -18,3 +25,30 @@ def plot_point_process_distribution(pts, mpi_size, colormap=cm.jet):
     for i in range(mpi_size):
         plt.scatter(pts[0, a[i][0]:a[i][1]], pts[1, a[i][0]:a[i][1]], c=colors[i])
     plt.show()
+
+def find_owner(filename):
+    # from https://stackoverflow.com/a/1830635
+    return getpwuid(os.stat(filename).st_uid).pw_name
+
+def clean_shared_memory():
+    '''
+    This function cleans up shared memory segments created by the GUI or a buggy Open MPI.
+    '''
+    # this only works for linux that has /dev/shm
+    if not sys.platform.startswith('linux'):
+        print("This function works only under Linux. Stop.", file=sys.stderr)
+        return
+    assert os.path.isdir('/dev/shm/')
+
+    shm_list = os.listdir('/dev/shm/')
+    user = getpass.getuser()   
+
+    owned_shm_list = []
+    for shm in shm_list:
+        if (shm.startswith('ptycho') or shm.startswith('vader')) \
+           and user == find_owner('/dev/shm/'+shm):
+            owned_shm_list.append(shm)
+
+    from posix_ipc import SharedMemory
+    for shm in owned_shm_list:
+        SharedMemory("/"+shm).unlink()
