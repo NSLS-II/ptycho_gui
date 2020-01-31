af39c66657285e69071e413f48a94db476a19eac
Leo Fang <leofang@bnl.gov>
2020-01-31 13:51:01 -0500

    support drag & drop in associate scan window
    TODO: clean up the useless code

--- nsls2ptycho/core/widgets/list_widget.py
+++ nsls2ptycho/core/widgets/list_widget.py
@@ -1,11 +1,37 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from nsls2ptycho.ui import ui_list_dialog
 
+
+#def _dragEnterEvent(listWidget, event):
+#    #event.setDropAction(QtCore.Qt.MoveAction)
+#    event.accept()
+#
+#def _dragMoveEvent(listWidget, event):
+#    #event.setDropAction(QtCore.Qt.MoveAction)
+#    event.accept()
+#
+#def _dropEvent(listWidget, event):
+#    #event.setDropAction(QtCore.Qt.CopyAction)
+#    event.accept()
+#    listWidget.addItem(str(event.mimeData().text()))
+
+
 class ListWidget(QtWidgets.QDialog, ui_list_dialog.Ui_Form):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
         QtWidgets.QApplication.setStyle('Plastique')
+
+        self.listWidget.setDragDropMode(QtWidgets.QAbstractItemView.DragDrop)
+        self.listWidget.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
+        #self.listWidget.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
+        self.listWidget.setAcceptDrops(True)
+        self.listWidget.setDragEnabled(True)
+        self.listWidget.setDropIndicatorShown(True)
+        self.listWidget.setDefaultDropAction(QtCore.Qt.MoveAction)
+        #self.listWidget.dragEnterEvent = lambda event: _dragEnterEvent(self.listWidget, event)
+        #self.listWidget.dragMoveEvent = lambda event: _dragMoveEvent(self.listWidget, event)
+        #self.listWidget.dropEvent = lambda event: _dropEvent(self.listWidget, event)
 
         self.btn_add_item.clicked.connect(self.add_item)
         self.btn_rm_item.clicked.connect(self.remove_item)
@@ -17,9 +43,9 @@ class ListWidget(QtWidgets.QDialog, ui_list_dialog.Ui_Form):
     def add_item(self):
         self.listWidget.addItem(str(self.le_input.text()))
         self.le_input.setText('')
-        self.listWidget.sortItems()
+        #self.listWidget.sortItems()
 
     def remove_item(self):
         item = self.listWidget.takeItem(self.listWidget.currentRow())
-        self.listWidget.sortItems()
+        #self.listWidget.sortItems()
         del item
